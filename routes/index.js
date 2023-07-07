44fc396e90d1c32fd5ea5ad88be71e139c3f1e77
irfanperveez <ipervez@leeddev.io>
2023-07-07 00:10:23 -0700

    Node modules ignore

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const path = require('path');
 const bodyparser = require("body-parser");
 const app = express();
 const files = require('../controllers/files');
+const middleware = require('../controllers/middleware');
 
-const upload = files.upload;
- 
 app.use(express.static("uploads"));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.get('/', files.homepage);
-app.post("/docxtopdf", upload.single("file"), files.uploadfile);
+app.post('/docxtopdf', middleware.uploadMiddleware, files.uploadfile);
+
  
 app.listen(5000, () => {
   console.log("App is listening on port 5000");
